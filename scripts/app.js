853c3cee64b288d2f13f87f8132b6845d2b5a2a4
Suleng18 <90331457+suleng18@users.noreply.github.com>
2022-10-25 22:01:17 +0700

    complete interface coding

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+$('.customer-list').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow: `<button type='button' class='slick-prev pull-left'><img src="./images/arrow-left.svg"/></button>`,
+  nextArrow: `<button type='button' class='slick-next pull-right'><img src="./images/arrow-right.svg"/></button>`,
+});
